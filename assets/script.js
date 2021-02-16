5415217fb8a81b790e8343164db83d8ab247f562
Anisha McDowell <anishamcdowell@gmail.com>
2021-02-15 22:24:24 -0500

    Randomize questions functions started. Still incomplete

--- assets/script.js
+++ assets/script.js
@@ -5,19 +5,37 @@ var quizEl = document.getElementById("quiz-container");
 
 //Quiz Results
 var totalQuestions = 10;
-var correctAnswers = userAnswers.value;
-var results = correctAnswers + "/" totalQuestions
+var questionsArray = [];
+var correctAnswers;
+var options = ["A", "B", "C", "D"];
+var results = "correctAnswers + "/" totalQuestions";
 
 //Initial app behavior
 nextButton.hidden = true;
+
+// function randomizeQuestions() {
+//     var randomQuestion = totalQuestions.Math.floor(Math.random() * 11);
+//     console.log(randomQuestion);
+// }
+
+//Randomize quiz questions
+function randomizeQuestions () {
+    for (var i = 0; i < totalQuestions.length; i++) {
+        questionsArray.push(Math.floor(Math.random() * (10 - 1) ) + 1);
+    }
+    console.log(questionsArray);
+}
 
 //Start Quiz
 startButton.addEventListener("click", function startQuiz() {
     startButton.hidden = true;
     nextButton.hidden = false;
+    //Randomize question order
+    randomizeQuestions();
 });
 
 //Quiz
+//Randomize answer to each question
 //Correct answer selected
 
 //Incorrect answer selection
@@ -31,6 +49,6 @@ nextButton.addEventListener("click", function nextQuestion() {
 //Final answer answered
 
 //Show results
-function showResults {
+function showResults() {
     document.getElementById("results").textContent=results;
 }
