3053988d7108e4038301220e277cf9d6cb4b79fb
Anisha McDowell <anishamcdowell@gmail.com>
2021-02-20 14:40:51 -0500

    Timer interval working

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,117 @@
 //Global variables
 var isStarted = false;
+var timeEl = document.getElementById("timer");
 
 //Button Elements
 var startButton = document.getElementById("start-quiz-btn");
 var nextButton = document.getElementById("next-btn");
 var quizEl = document.getElementById("quiz-container");
 var questionEl = document.getElementById("question-container");
+var scoresButton = document.getElementById("view-highscores");
+var retryButton = document.getElementById("retry-quiz");
+
+//Hidden Message
+
+var timeUpMsg = document.getElementById("hidden-msg");
+console.log(timeUpMsg);
 
 //Quiz Results
-var totalQuestions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(totalQuestions)
+// var totalQuestions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(totalQuestions)
 
-var randomQuestionOrder = [];
-var correctAnswers;
-var options = ["A", "B", "C", "D"];
+// var randomQuestionOrder = [];
+// var correctAnswers;
+// var options = ["A", "B", "C", "D"];
 
-var answerA = document.getElementById("answer-btn-a");
-var answerB = document.getElementById("answer-btn-b");
-var answerC = document.getElementById("answer-btn-c");
-var answerD = document.getElementById("answer-btn-d");
+// var answerA = document.getElementById("answer-btn-a");
+// var answerB = document.getElementById("answer-btn-b");
+// var answerC = document.getElementById("answer-btn-c");
+// var answerD = document.getElementById("answer-btn-d");
 
-var results = "correctAnswers + "/" totalQuestions";
+// var results = "correctAnswers + "/" totalQuestions";
 
 //Initial app behavior
 nextButton.hidden = true;
 if (isStarted === false) {
     quizEl.style.display = "none";
     nextButton.style.display = "none";
+    timeUpMsg.style.display = "none";
+    scoresButton.style.display = "none";
+    retryButton.style.display = "none";
 }
 
-
-
-//Randomize quiz questions
-// function randomizeQuestions() {
-//}
-
-//Start Quiz
-function startQuiz() {
+//Start Quiz and Timer
+startButton.addEventListener("click", function startQuiz() {
     startButton.style.display = "none";
     quizEl.style.display = "inline-block";
     nextButton.style.display = "inline-block";
-    questionEl.textContent = questionsArray.questions[0];
-    answerA.textContent = answersArray.correctAnswers[0];
+    showTimer();
+});
+
+time = 3;
+
+function showTimer() {
+    var timerInterval = setInterval(function() {
+        time--;
+        timeEl.textContent = "Timer: " + time;
+        if (time == 0) {
+            clearInterval(timerInterval);
+            //Display "Time's up!" message
+            nextButton.style.display ="none";
+            quizEl.style.display ="none";
+            timeUpMsg.style.display = "inline-block";
+            scoresButton.style.display = "block";
+            retryButton.style.display = "block";
+        }
+    }, 1000);
 };
 
-console.log(answerA);
+//View my score buttonn function
+
+//Retry quiz button function
+function retry() {
+    retryButton.addEventListener("click, function(){
+        location.reload();
+    }")
+}
+
+
+// //Load Questions
+// function loadQuestions() {
+//     // questionEl.textContent = questionsArray[0];
+//     // answerB.textContent = answersArray[0];
+//     // answerA.textContent = optionsArray[0][0];
+//     // answerC.textContent = optionsArray[0][1];
+//     // answerD.textContent = optionsArray[0][2];
+// }
 
 //Quiz
-var questionsArray = {
-    questions: ["What is 1+1?", "Q2", "Q3", "Q4", "Q5"]
-};
-var answersArray = {
-    correctAnswers: ["2", "A2", "A3", "A4", "A5"],
-};
-var optionsArray = {
-    options1: ["opt1", "opt2", "opt3", "opt4", "opt5"],
-    options2: ["opt1", "opt2", "opt3", "opt4", "opt5"],
-    options3: ["opt1", "opt2", "opt3", "opt4", "opt5"],
-    options4: ["opt1", "opt2", "opt3", "opt4", "opt5"],
-    options5: ["opt1", "opt2", "opt3", "opt4", "opt5"],
-};
+var questionsArray = [
+    "What does HTML stand for?", 
+    "Which is the correct CSS syntax?", 
+    "Which of this is used to link a Javascript document to an HTML document?", 
+    "Which semantic tag can replace a generic <div> when creating a navigation bar?", 
+    "Which language is used to style the UI of an HTML document?"
+];
+
+var answersArray = [
+    "Hypertext Markup Language", 
+    "font-size: 12px;", 
+    "<script></script>", 
+    "<nav>", 
+    "CSS"
+];
+
+var optionsArray = [
+    ["Hypertext Marked Language", "Hyper Typer Mark Language", "Hypertext Marking Language"],
+    ["font-size: 12px;", "font-size: 12;", "text-size: 12px;", "font: 12px;"],
+    ["<js></js>", "<link></link>", "<a></a>", "<script></script>"],
+    ["<navbar>", "<navigate>", "<nav>", "<navigation>"],
+    ["CSS", "Javascript", "Java", "Python"],
+];
 
 var randomQuestion = Math.floor(Math.random() * 5); 
-console.log(optionsArray.options1[randomQuestion]);
+console.log(optionsArray[randomQuestion]);
 
 //Randomize answer to each question
 //Correct answer selected
@@ -77,7 +126,7 @@ nextButton.addEventListener("click", function nextQuestion() {
 //End of quiz
 //Final answer answered
 
-//Show results
-function showResults() {
-    document.getElementById("results").textContent=results;
-}
+// //Show results
+// function showResults() {
+//     document.getElementById("results").textContent=results;
+// }
